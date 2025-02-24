02af688baedd5b7fa51f3a55e5e51c1b44873669
CNyaha <llhb0625@gmail.com>
2025-02-24 11:33:29 +0900

    docs_파일 이름 수정

--- Assets/Script/Weapon/ProjectileController.cs
+++ Assets/Script/Weapon/ProjectileController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript9 : MonoBehaviour
+public class ProjectileController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
