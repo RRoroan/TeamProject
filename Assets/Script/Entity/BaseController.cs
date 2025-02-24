7a5cc4bfcce30f58329fbaa030a8c64f18d2e9b7
Chanbamx2 <ych7669@naver.com>
2025-02-24 20:32:56 +0900

    [Fix] 회전 방식 및 무기 위치 수정

--- Assets/Script/Entity/BaseController.cs
+++ Assets/Script/Entity/BaseController.cs
@@ -9,7 +9,7 @@ public class BaseController : MonoBehaviour
     protected Rigidbody2D _rigidbody;
 
     [SerializeField] private SpriteRenderer characterRenderer;
-    [SerializeField] private Transform weaponPivot;
+    [SerializeField] public Transform weaponPivot;
 
     protected Vector2 movementDirection = Vector2.zero;
     public Vector2 MovementDirection { get { return movementDirection; } }
@@ -95,52 +95,20 @@ public class BaseController : MonoBehaviour
     private void Rotate(Vector2 direction)
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
-        float weaponRotZ = Mathf.Atan2(
-            TrankingEnemy().position.y - transform.position.y
-            ,TrankingEnemy().position.x - transform.position.x
-            ) * Mathf.Rad2Deg - 90;
+       
         float t = 10f;
 
         transform.rotation = Quaternion.Lerp(
             transform.rotation, Quaternion.Euler(0,0,rotZ), Time.deltaTime * t);
 
-        if (weaponPivot != null)
-        {
-            weaponPivot.rotation = Quaternion.Lerp(
-                weaponPivot.rotation, Quaternion.Euler(0, 0, weaponRotZ), Time.deltaTime * t);
-        }
+        //if (weaponPivot != null)
+        //{
+        //    weaponPivot.rotation = Quaternion.Lerp(
+        //        weaponPivot.rotation, Quaternion.Euler(0, 0, weaponRotZ), Time.deltaTime * t);
+        //}
     }
 
-    private Transform TrankingEnemy()   // 가까운 적 추적
-    {
-        int enemyCount = Enemy.transform.childCount;    // 적의 수
-        List<Transform> enemy = new List<Transform> { };    // 적 리스트
-        List<float> enemyPos = new List<float> { };     // 적 위치 리스트
-        
-        for (int i = 0; i < enemyCount; i++)        // 적 리스트에 추가
-        {
-            enemy.Add(Enemy.transform.GetChild(i));
-        }
 
-        foreach (Transform t in enemy)              // 적 위치 리스트에 추가
-        {
-            enemyPos.Add(Vector2.Distance(t.position, transform.position));
-        }
-
-        float closesDistance = float.MaxValue;      // 가장 가까운 거리
-        Transform closestEnemy = null;              // 가장 가까운 적
-
-        for (int i = 0; i < enemyPos.Count; i++)
-        {
-            if (enemyPos[i] < closesDistance)
-            {
-                closesDistance = enemyPos[i];
-                closestEnemy = enemy[i];
-            }
-        }
-
-        return closestEnemy;
-    }
 
     public void ApplyKnockback(Transform other, float power, float duration)
     {
