c862d49918cf15102b59997edd5c0ad8528546d7
Chanbamx2 <ych7669@naver.com>
2025-02-24 12:13:55 +0900

    [Feat] 플레이어 이동, 회전

--- Assets/Script/Entity/BaseController.cs
+++ Assets/Script/Entity/BaseController.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRanderer;
+    [SerializeField] private Transform weaponPivot;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0f;
+
+    protected virtual void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Start()
     {
-        
+
+    }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate(movementDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+        if (knockbackDuration > 0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * 5f;
+        if (knockbackDuration > 0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+
+        _rigidbody.velocity = direction;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+
+        if (weaponPivot != null)
+        {
+            weaponPivot.rotation = Quaternion.Euler(0f, 0f, rotZ);
+        }
+    }
+
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        knockback = -(other.position - transform.position).normalized * power;
     }
 }
