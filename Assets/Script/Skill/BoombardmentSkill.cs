245f7da80215c185a65f91d3cd36b48083612dfb
CNyaha <llhb0625@gmail.com>
2025-02-25 21:06:03 +0900

    Feat_포격 스킬 완성

--- Assets/Script/Skill/BoombardmentSkill.cs
+++ Assets/Script/Skill/BoombardmentSkill.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.GameCenter;
 using UnityEngine.UIElements;
@@ -18,6 +19,10 @@ public class BoombardmentSkill : BaseSkill
     [SerializeField] private float skillSize = 0.5f;
     // 사라질 시간
     [SerializeField] private float lifetime = 1f;
+    // 스킬의 쿨다운
+    [SerializeField] private float cooldown = 3f;
+
+    private bool isColldown = false;
 
     private Vector2 mapMinBounds;
     private Vector2 mapMaxBounds;
@@ -33,11 +38,23 @@ public class BoombardmentSkill : BaseSkill
 
     public override void UseSkill()
     {
+        if (isColldown) return;
+        StartCoroutine(SkillColldown());
         Vector2 randomPosition = RandomBoombardPosition(player.transform.position);
-        Instantiate(boombardPrefabs, randomPosition, Quaternion.identity);
+        GameObject boombard = Instantiate(boombardPrefabs, randomPosition, Quaternion.identity);
 
-        Destroy(boombardPrefabs, lifetime);
+        boombard.transform.localScale = new Vector3(skillSize, skillSize, 1f);
 
+        Destroy(boombard, lifetime);
+
+    }
+
+    // 스킬의 쿨다운이 되었을 때 사용 가능하게
+    private IEnumerator SkillColldown()
+    {
+        isColldown = true;
+        yield return new WaitForSeconds(cooldown);
+        isColldown = false;
     }
 
     private Vector2 RandomBoombardPosition(Vector2 playerPosition)
@@ -45,8 +62,8 @@ public class BoombardmentSkill : BaseSkill
 
         Vector2 randomDirection = Random.insideUnitCircle.normalized;
         // minRange ~ maxRange 사이의 랜덤 포격 위치를 설정
-        float randomDistance = Random.RandomRange(minRange, maxRange);
-        Vector2 targetPosition = playerPosition + randomDirection * randomDirection;
+        float randomDistance = Random.Range(minRange, maxRange);
+        Vector2 targetPosition = playerPosition + randomDirection * randomDistance;
 
         // 스킬이 맵 밖을 안나가도록 조정
         float adjustX = Mathf.Clamp(targetPosition.x, mapMinBounds.x + skillSize, mapMaxBounds.x - skillSize);
