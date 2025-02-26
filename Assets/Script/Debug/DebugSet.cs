89d699d17b7359785fb3b3df1728c363f8b0e41a
CaveTiger <kinusagi99@gmail.com>
2025-02-26 16:46:24 +0900

    디버그룸 세팅

---
+++ Assets/Script/Debug/DebugSet.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugSet : MonoBehaviour
+{
+    // 디버그의 핵심 디버그에 필요한걸 찾아보자.
+    private Player player;
+}
