204b92ce6d5bf680e626c94b92ac917c1287cbb7
RRoroan <tjdals8788@gmail.com>
2025-02-27 11:41:38 +0900

    [Fix] fix bugs for merging to dev

--- Assets/Script/Skill/BaseSkill.cs
+++ Assets/Script/Skill/BaseSkill.cs
@@ -13,7 +13,7 @@ public abstract class BaseSkill : MonoBehaviour
     public string RequiredItem;
 
     protected StatHandler statHandler;
-    protected Player player;
+    protected PlayerController playerController;
     protected MapSizeDetecte mapSize;
 
     // 투사채 발사 위치(시작 위치)
@@ -26,21 +26,21 @@ public abstract class BaseSkill : MonoBehaviour
 
     public void Awake()
     {
-        player = FindObjectOfType<Player>();
+        playerController = FindObjectOfType<PlayerController>();
     }
 
     protected virtual void Start()
     {
         statHandler = GameManager.Instance.GetStatHandler();
         mapSize = GameManager.Instance.mapSize;
-        firePoint = player.transform;
+        firePoint = playerController.transform;
 
         // 맵의 좌하단 좌표
         mapMinBounds = mapSize.GetMinBounds();
         // 맵의 우상단 좌표
         mapMaxBounds = mapSize.GetMaxBounds();
 
-        if (player == null)
+        if (playerController == null)
         {
             Debug.Log("플레이어가 존재하지 않습니다.");
         }
